98ffc323824e77a810f18fa4958392a40a012dec
SayWess <saywess.developpement@gmail.com>
2025-01-19 22:53:15 +0100

    Add temporary add video button and use asyncStorage reset button

--- frontend/mobile/YouSave/app/home.tsx
+++ frontend/mobile/YouSave/app/home.tsx
@@ -1,8 +1,38 @@
-import { TextInput, Image, StyleSheet, View, ActivityIndicator, TouchableOpacity } from "react-native";
+import { TextInput, Image, StyleSheet, View, ActivityIndicator, TouchableOpacity, Button } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { useThemeColors } from "@/hooks/useThemeColors";
-import { useState } from "react";
 
+import ResetButton from "@/store/reset";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/store/store";
+import { addVideo } from "@/store/videosSlice";
+
+
+interface Video {
+  id: string;
+  title: string;
+  thumbnail: string;
+  state: "idle" | "downloading" | "downloaded";
+  url: string;
+}
+
+const initialVideos: Video[] = [
+  {
+    id: "1",
+    title: "泣きながら「ドライフラワー」歌ってみた【こはならむ】",
+    thumbnail: "https://via.placeholder.com/150",
+    state: "idle",
+    url: "https://www.youtube.com/watch?v=33_zCbUfDog&list=RDGMEMhCgTQvcskbGUxqI4Sn2QYw&index=27",
+  },
+  {
+    id: "2",
+    title:
+      "YOASOBI「勇者」 Official Music Video／TVアニメ『葬送のフリーレン』オープニングテーマ",
+    thumbnail: "https://via.placeholder.com/150",
+    state: "idle",
+    url: "https://www.youtube.com/watch?v=OIBODIPC_8Y&list=RD2eOg5DoYuwU&index=32",
+  },
+];
 
 interface HomeProps {
     url: string, 
@@ -15,10 +45,16 @@ export default function Home({ url, setUrl, loading, setLoading }: HomeProps) {
 
     const colors = useThemeColors();
 
+    const dispatch: AppDispatch = useDispatch();
+
     const handleSubmit = () => {
         if (!url.trim()) return;
         setLoading(true);
-    
+
+        console.log("Adding video", initialVideos[0])
+        dispatch(addVideo(initialVideos[0]));
+        // dataManager.addVideo(initialVideos[0]);
+        // dataManager.clearVideos();
         // Simulate a delay for submission (replace with actual logic)
         setTimeout(() => {
           setLoading(false);
@@ -58,7 +94,11 @@ export default function Home({ url, setUrl, loading, setLoading }: HomeProps) {
                 <ThemedText variant="button" color="white">Submit</ThemedText>
               )}
             </TouchableOpacity>
+
+            <ResetButton />
+
         </View>
+        
     )
 }
 
